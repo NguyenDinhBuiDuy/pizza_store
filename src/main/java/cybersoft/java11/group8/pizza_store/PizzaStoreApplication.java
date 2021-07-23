ba3a359c55918fb88f0f1d6990b1f2f443b47877
nguyenduy <thienduybmt25021997@gmail.com>
2021-07-23 15:34:52 +0700

    Initial commit

---
+++ src/main/java/cybersoft/java11/group8/pizza_store/PizzaStoreApplication.java
@@ -0,0 +1,13 @@
+package cybersoft.java11.group8.pizza_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cybersoft/java11/group8/pizza_store/PizzaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package cybersoft.java11.group8.pizza_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
